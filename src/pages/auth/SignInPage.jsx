506a95d05d006b591fd4d00012a7e73b4ef0e2c3
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-08-07 16:54:53 -0700

    Finished SignInPage, SignUpPage working on FormContainer - have log & form next add image

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,16 +1,22 @@
+import { Link } from 'react-router-dom';
 import AuthForm from './AuthForm.jsx';
+import FormContainer from './AuthForm.jsx/FormContainer.jsx';
 
 const SignInPage = () => {
   return (
-    <div className='font-lato text-slate-900 font-bold border border-blue-500 flex justify-center items-center'>
-      <AuthForm
-        fields={[
-          { label: 'username', type: 'text' },
-          { label: 'password', type: 'password'},
-        ]}
-                submitButtonLabel="Sign In"
-
-      />
+    <div className='font-playfair text-slate-800 font-medium flex justify-center items-center'>
+      <FormContainer>
+        <AuthForm
+          fields={[
+            { label: 'username', type: 'text' },
+            { label: 'password', type: 'password' },
+          ]}
+          submitButtonLabel='Sign In'
+        />
+        <Link className='flex justify-center text-emerald-500 underline' to="/signup">
+          create an account
+        </Link>
+      </FormContainer>
     </div>
   );
 };
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,19 +1,22 @@
+import { Link } from 'react-router-dom';
 import AuthForm from './AuthForm.jsx';
+import FormContainer from './AuthForm.jsx/FormContainer.jsx';
 
 const SignUpPage = () => {
   return (
-    <div className='font-playfair text-slate-800 font-medium border border-red-500 flex justify-center items-center'>
-      <AuthForm 
-        fields={
-          ([
+      <div className='font-playfair text-slate-800 font-medium flex justify-center items-center'>
+    <FormContainer>
+        <AuthForm
+          fields={[
             { label: 'username', type: 'text' },
             { label: 'password', type: 'password' },
-            { label: 'confirm password',  type: 'password' },
-          ])
-        }
-        submitButtonLabel="Create Account"
-      />
-    </div>
+            { label: 'confirm password', type: 'password' },
+          ]}
+          submitButtonLabel='Create Account'
+        />
+    <Link className="flex justify-center text-emerald-500 font-playfair underline" to="/">sign-in</Link>
+    </FormContainer>
+      </div>
   );
 };
 
