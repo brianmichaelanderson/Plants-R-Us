abe4e41c758ab768d3d709ee50356c26e7857234
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-08-09 07:35:40 -0700

    implemented useNavigate if account creation successful

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,10 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import AuthForm from './AuthForm.jsx';
 import FormContainer from './AuthForm.jsx/FormContainer.jsx';
 
 const SignInPage = () => {
+    const location = useLocation();
+
   return (
       <FormContainer>
+        {/* use the 'optional chaining operator' to return undefined instead of throwing a run-time error if location.state is undefined or null. */}
+        {location.state?.accountCreated && <div className="border border-emerald-700 rounded-md flex justify-center m-4 text-emerald-800 bg-emerald-100 p-2">Account created! Please sign in.</div>}
         <AuthForm
           fields={[
             { label: 'username', type: 'text' },
