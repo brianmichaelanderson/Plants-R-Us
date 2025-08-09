b953267af433064aab6c66e57fee034e48121e02
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-08-09 14:48:18 -0700

    Resolved SessionStorage issues mostly syntax and started setting up pulling username from the decoded token

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
 import SignInPage from './pages/auth/SignInPage';
 import SignUpPage from './pages/auth/SignUpPage';
+import SessionContext from 'contexts/SessionContext';
+import * as userService from 'services/user';
 
 const App = () => {
+  //looks in local storage to return a token or return null for the initial sessionToken state.
+  const [sessionToken, setSessionToken] = useState(() =>
+    userService.getSessionTokenStorage()
+  );
+
   return (
-    <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<SignInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-     </Routes>
-    </BrowserRouter>
+    <SessionContext.Provider
+      value={{
+        //decode sessionToken then pull the username from it
+        username: jwtDecode(sessionToken).username,
+        signIn: (token) => {
+          setSessionToken(token);
+          userService.setSessionTokenStorage(token);
+        },
+        signOut: () => {
+          setSessionToken(null);
+          userService.removeSessionTokenStorage()
+        }
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SignInPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </SessionContext.Provider>
   );
 };
 
--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,16 +1,23 @@
 import { Link, useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import AuthForm from './AuthForm.jsx';
 import FormContainer from './AuthForm.jsx/FormContainer.jsx';
 import * as userService from 'services/user.js';
+import SessionContext from 'contexts/SessionContext.js';
+
 
 const SignInPage = () => {
   const [error, setError] = useState('');
   const location = useLocation();
+  const sessionContext = useContext(SessionContext);
 
   return (
     <FormContainer>
-      {error && <div className='text-center text-red-600 font-lato mt-4 mb-3'>{error}</div>}
+      {error && (
+        <div className='text-center text-red-600 font-lato mt-4 mb-3'>
+          {error}
+        </div>
+      )}
       {/* use the 'optional chaining operator' to return undefined instead of throwing a run-time error if location.state is undefined or null. */}
       {location.state?.accountCreated && (
         <div className='border border-emerald-700 rounded-md flex justify-center m-4 text-emerald-800 bg-emerald-100 p-2'>
@@ -29,11 +36,13 @@ const SignInPage = () => {
             username: values.username,
             password: values.password,
           });
+          const data = await response.json();
+          console.log(data);
           if (response.status === 201) {
+            sessionContext.signIn(data.capstone_session_token);
             console.log('Signed in');
-            setError('')
+            setError('');
           } else {
-            const data = await response.json();
             setError(data.error);
           }
         }}
