c458ae856604f0f54b2d74ca11be7878c3fd98a3
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-08-10 13:32:50 -0700

    completed Fetching plants services confirming status 200

--- src/pages/PlantListPage.jsx/index.jsx
+++ src/pages/PlantListPage.jsx/index.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
+import * as plantService from 'services/plant';
 import NavBar from 'shared-components/NavBar.jsx';
 
 const PlantListPage = () => {
+  const getAllPlants = async () => {
+      const response = await plantService.getPlants()
+      const data = await response.json()
+      console.log('response.status = ', response.status)
+      console.log('data = ', data)
+  }
+
+  useEffect(() => {
+    getAllPlants();
+    //Below lets everything reside in the useEffect, which doesn't allow async
+    //putting another anonymous function wrapped in () can be set as async with it's () followed by another set of () to invoke it
+    // (async () => {
+    //   const response = await plantService.getPlants();
+    //   console.log('getPlants response =', response.status);
+    // })();
+  }, []);
+
   return (
     <div>
       <NavBar />
