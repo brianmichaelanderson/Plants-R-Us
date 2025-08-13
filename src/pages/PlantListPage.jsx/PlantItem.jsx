d8145bd51172deb51ea7a4b3f3964bf9c4330bac
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-08-13 15:47:02 -0700

    Completed onMouseEnter & color name change and outline color

--- src/pages/PlantListPage.jsx/PlantItem.jsx
+++ src/pages/PlantListPage.jsx/PlantItem.jsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx';
+import { useState } from 'react';
 
 const PlantItem = (props) => {
   const { plant } = props;
-
+  const [imageIdx, setImageIdx] = useState(0);
 
   const POT_COLORS = {
     stone: 'bg-stone-200',
@@ -19,23 +20,25 @@ const PlantItem = (props) => {
     <div className='mr-8 mb-5'>
       <img
         className='w-[280px] h-[320px] rounded-md'
-        src={plant.images[0].src}
+        src={plant.images[imageIdx].src}
       />
       <div className='flex justify-between items-center mt-3 text-emerald-700 text-xl'>
         <div className='font-playfair'>{plant.name}</div>
         <div className='flex items-center'>${plant.price}</div>
       </div>
       <div className='flex justify-between items-center mt-2'>
-        <div className='text-slate-500'>color</div>
+        <div className='text-slate-500'>{plant.images[imageIdx].pot_color}</div>
         <div className='flex'>
           {/* get pot_color name to look up as key in POT_COLORS for TWCSS bg styling */}
           {plant.images.map((imageElement, idx) => (
             <div
               key={idx}
               className={clsx(
-                'ml-1 h-5 w-5 rounded-full border border-slate-500',
-                POT_COLORS[imageElement.pot_color], 
+                'ml-[3px] h-5 w-5 rounded-full border border-slate-500',
+                POT_COLORS[imageElement.pot_color],
+                imageIdx === idx && 'outline outline-slate-400 outline-offset-2'
               )}
+              onMouseEnter={() => setImageIdx(idx)}
             ></div>
           ))}
         </div>
