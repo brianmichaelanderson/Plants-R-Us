90af65094fc65f504989f9383a840ce0cacf8aef
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-08-21 12:26:44 -0700

    completed populating CartModal with PantItems & their styling

--- src/shared-components/NavBar.jsx/modals/CartModal/CartItem.jsx
+++ src/shared-components/NavBar.jsx/modals/CartModal/CartItem.jsx
@@ -4,18 +4,23 @@ const CartItem = (props) => {
   return (
     <div className='flex m-4'>
       <img className='w-28 rounded-md' src={item.image_src} />
-      <div className='mx-4 flex-1'>
-        <div className='text-emerald-700 font-playfair flex justify-between'>
-          <div className=''>{item.plant_name}</div>
-          <div className='text-slate-500 pr-8'>
-            ${item.price_per_unit * item.quantity}
+      <div className='mx-4 flex justify-between flex-1'>
+        <div>
+          <div className='text-emerald-700 font-playfair'>
+            <div className=''>{item.plant_name}</div>
+          </div>
+          <div className='flex text-slate-500'>
+            <div className="w-12 text-slate-400">qty:</div>
+            <div>{item.quantity}</div>
+          </div>
+          <div className='flex text-slate-500'>
+            <div className="w-12 text-slate-400">color: </div>
+            <div>{item.pot_color}</div>
           </div>
         </div>
-        <div className="flex">
-          <div>color: {item.pot_color}</div>
-        </div>
-          <div>qty:</div>
-          <div>{item.quantity}</div>
+      </div>
+      <div className='text-slate-500 pr-8'>
+        ${item.price_per_unit * item.quantity}
       </div>
     </div>
   );
