764fb9ad6b1105ec743b067b1d83066b5b2a2fd6
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-08-23 09:18:28 -0700

    Got total cart items and cart subtotal completed

--- src/shared-components/NavBar.jsx/modals/CartModal/index.jsx
+++ src/shared-components/NavBar.jsx/modals/CartModal/index.jsx
@@ -7,20 +7,17 @@ import LoadingSpinner from 'shared-components/LoadingSpinner';
 
 export const CartModal = (props) => {
   const { setCartModalOpen } = props;
-  const [cartItems, setCartItems] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { username } = useContext(SessionContext);
 
-  //useCallback meoizes the function so its reference stays stable
-  //across re-renders (unless dependencies change).
-  //Useful when passing the function to useEffect
-  //It prevents unnecessary re-renders caused by a new function reference - can cause infinite loops in useEffect.
+  //useCallback meoizes the function so its reference stays stable across re-renders (unless dependencies change). Useful when passing the function to useEffect. It prevents unnecessary re-renders caused by a new function reference - can cause infinite loops in useEffect.
   const fetchCart = useCallback(async () => {
     setIsLoading(true);
     const response = await cartService.getCart();
     console.log('getCart response.status', response.status);
     const data = await response.json();
-    console.log('CartModel.jsx data from response.json() = ', data);
+    console.log('CartModal.jsx data from response.json() = ', data);
     setCartItems(data);
     setIsLoading(false);
   }, []);
@@ -29,6 +26,15 @@ export const CartModal = (props) => {
     fetchCart();
   }, [fetchCart]);
 
+ 
+    let totalItems = 0;
+    let subTotal =0;
+    for (let item of cartItems) {
+      totalItems += item.quantity;
+      subTotal += item.quantity * item.price_per_unit
+    }
+
+  
   return (
     <RemoveScroll>
       <div className='fixed left-0 top-0 w-full h-screen bg-black/30 flex justify-end backdrop-blur-sm'>
@@ -57,11 +63,11 @@ export const CartModal = (props) => {
                 ))}
           </div>
             <div className='flex flex-col border-t-2 border-slate-300 mx-4 my-10 '>
-              <div className='text-slate-400 flex justify-between mx-8 my-4'>
-                <div>7 items</div>
+              <div className='text-slate-400 flex justify-between items-center mx-8 my-4'>
+                <div>{totalItems} items</div>
                 <div>
                   subtotal:
-                  <span className='text-slate-600 pl-2 text-lg'>$550</span>
+                  <span className='text-slate-600 pl-2 text-lg'>${subTotal}</span>
                 </div>
               </div>
               <button className='bg-emerald-600 flex justify-center py-4 mx-8 rounded-full items-center'>
@@ -80,8 +86,3 @@ export const CartModal = (props) => {
 
 export default CartModal;
 
-// {!isLoading && cartItems.map((item, idx) => {
-// <div key={[0].item.id}>
-//   {item.id}
-// </div>
-// })}
